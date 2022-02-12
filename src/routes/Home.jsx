a285768d80258ca90dc59839ce7d5483822dcea5
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-02-11 22:26:28 -0800

    finished contact section and improved other ui components

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import "../styles/Home.css";
 import AboutMe from "../components/AboutMe";
 import MyWork from "../components/MyWork";
+import ContactMe from "../components/ContactMe";
 
 function Home() {
   return (
     <div className="App">
       <AboutMe />
       <MyWork />
+      <ContactMe />
     </div>
   );
 }
